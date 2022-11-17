40350c577788b221196e8dad628eb5c67914eca9
chuk-chuk <yulia.litvinchuk@alpima.com>
2022-11-17 12:16:12 +0000

    clean up

--- src/features/posts/fetchPosts.ts
+++ src/features/posts/fetchPosts.ts
@@ -1,14 +1,15 @@
 import { Post } from "../../types";
 import { getPostsFailure, getPostsLoading, getPostsSuccess } from "./postSlice";
 
+const baseUrl = "https://jsonplaceholder.typicode.com";
+
 export const fetchPosts = () => async (dispatch: any) => {
   try {
     dispatch(getPostsLoading(true));
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const response = await fetch(`${baseUrl}/posts`);
     const data: Post[] = await response.json();
     dispatch(getPostsSuccess(data));
   } catch (error: any) {
-    console.log(error);
     dispatch(getPostsFailure(error));
   } finally {
     dispatch(getPostsLoading(false));
